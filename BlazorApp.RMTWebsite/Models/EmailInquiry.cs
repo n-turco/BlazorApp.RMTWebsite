f5c640a5ac9515377ebbf3cd58afacb14170285e
n-turco <nicholas.turco@hotmail.com>
2026-07-21 10:14:56 -0400

    -updated error messages to reflect the issue more accuractely to the user

--- BlazorApp.RMTWebsite/Models/EmailInquiry.cs
+++ BlazorApp.RMTWebsite/Models/EmailInquiry.cs
@@ -6,18 +6,22 @@ namespace BlazorApp.RMTWebsite.Models
 {
     public class EmailInquiry : IEmailSender
     {
-        [Required]
-        [EmailAddress]
-        public string EmailAddress { get; set; } = "";
-        [Required]
-        public string EmailSubject { get; set; } = "";
+        [Required(ErrorMessage = "Email Address is required.")]
+        [EmailAddress(ErrorMessage = "Must be a valid email address.")]  
+        public string EmailAddress { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Subject line is required.")]
+        [StringLength(30, ErrorMessage = "Subject line is too long.")]
+        public string EmailSubject { get; set; } = string.Empty;
 
-        [Required]
-        public string EmailContent { get; set; } = "";
-        [Required]
-        public string FirstName { get; set; } = "";
-        [Required]
-        public string LastName { get; set; } = "";
+        [Required(ErrorMessage = "Message is required.")]
+        [StringLength(1000, ErrorMessage = "Message is too long, must be less than 1000 characters.")]
+        public string EmailContent { get; set; } = string.Empty;
+        [Required(ErrorMessage = "First Name is required.")]
+        [StringLength(30, ErrorMessage = "First name is too long.")]
+        public string FirstName { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Last Name is required.")]
+        [StringLength(30, ErrorMessage = "Last name is too long.")]
+        public string LastName { get; set; } = string.Empty;
 
         // Implement the interface method
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
