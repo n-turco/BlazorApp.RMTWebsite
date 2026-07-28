2bba6b97204a8f9ca70dce6439196094d97867cf
n-turco <nicholas.turco@hotmail.com>
2026-07-28 15:40:44 -0400

    fixed validation issues

--- BlazorApp.RMTWebsite/RMTServices/EmailSenderService.cs
+++ BlazorApp.RMTWebsite/RMTServices/EmailSenderService.cs
@@ -4,6 +4,9 @@ using Microsoft.Extensions.Options;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
 
 namespace BlazorApp.RMTWebsite.RMTServices
 {
@@ -39,8 +42,23 @@ namespace BlazorApp.RMTWebsite.RMTServices
             }
 
             await _httpClient.SendAsync(request);
-
-         
         }
+        //public static async Task<RestResponse> Send()
+        //{
+        //    var options = new RestClientOptions("https://api.mailgun.net")
+        //    {
+        //        Authenticator = new HttpBasicAuthenticator("api", Environment.GetEnvironmentVariable("API_KEY") ?? "API_KEY")
+        //    };
+
+        //    var client = new RestClient(options);
+        //    var request = new RestRequest("/v3/sandbox29ee27561c3e452ea58e840ba561dcb8.mailgun.org/messages", Method.Post);
+        //    request.AlwaysMultipartFormData = true;
+        //    request.AddParameter("from", "Mailgun Sandbox <postmaster@sandbox29ee27561c3e452ea58e840ba561dcb8.mailgun.org>");
+        //    request.AddParameter("to", "Nicholas Turco <nicholas.turco@hotmail.com>");
+        //    request.AddParameter("subject", "Hello Nicholas Turco");
+        //    request.AddParameter("text", "Congratulations Nicholas Turco, you just sent an email with Mailgun! You are truly awesome!");
+        //    return await client.ExecuteAsync(request);
+        //}
+
     }
 }
